bf88842445630bbbc968eb4a0ab5763b25d0977b
Milan G <dev@milangress.de>
2020-04-19 21:38:53 +0200

    1st draft

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Version 4-3</router-link> |
+      <router-link to="/">Version 6-3</router-link>
     </div>
     <router-view/>
   </div>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,174 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="paintControllBar">
+      <div v-on:click="setToRandomNewColor()"
+           v-bind:style="{background: 'rgb(' + this.currentBrushColor.join() + ')'}"
+           class="squareButton">new</div>
+      <input type="range" orient="vertical" v-model="strokeWidth"/>
+      <!--<input type="color" value="#ff0000">-->
+    </div>
+    <vue-p5 @setup="setup"
+            @draw="draw"
+            @keypressed="keypressed"
+            @mousedragged="mousedragged"></vue-p5>
+    <div class="colorBars">
+      <div v-for="(item) in imageData" :key="item.color" class="colorBarWrapper">
+        <div v-bind:style="{ background: item.color, width: item.percent + '%'}" class="colorBar">
+          <p>{{item.val}}</p>
+        </div>
+        <!--{{ val[0].split(";") }}-->
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+//import HelloWorld from '@/components/HelloWorld.vue'
+import VueP5 from 'vue-p5'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  components: {VueP5},
+  data: function() {
+    return {
+        imageData: [{ "color": "rgb(0,128,0)", "val": 259188 }],
+        width: 500,
+        height: 650,
+        strokeWidth: 4,
+        nextComputePixel: true,
+        currentBrushColor: [0,128,0]
+      }
+    },
+  mounted() {
+    setInterval(this.update, 500);
+    this.setToRandomNewColor();
+  },
+  methods: {
+    update: function() {
+      this.nextComputePixel = true
+    },
+    setup(sk) {
+      sk.createCanvas(this.width,this.height);
+      sk.pixelDensity(1)
+      sk.background('green')
+      //sk.text('Hello p5!', 20, 20)
+      sk.strokeWeight(4)
+    },
+    draw(sk) {
+      sk.stroke(sk.color(this.currentBrushColor[0], this.currentBrushColor[1], this.currentBrushColor[2]))
+      sk.strokeWeight(this.strokeWidth)
+      if (this.nextComputePixel) {
+        this.computePixels(sk)
+        this.nextComputePixel = false
+      }
+      //sk.updatePixels();
+    },
+    mousedragged(sk) {
+      sk.line(sk.pmouseX, sk.pmouseY, sk.mouseX, sk.mouseY)
+    },
+    keypressed(sk) {
+      sk.stroke(sk.color(sk.random(255),sk.random(255),sk.random(255)))
+    },
+    setToRandomNewColor(){
+      console.log('newcolor')
+      this.currentBrushColor = [
+              Math.floor(Math.random() * 255),
+              Math.floor(Math.random() * 255),
+              Math.floor(Math.random() * 255)
+      ]
+      debugger
+    },
+    computePixels(sk) {
+      console.log('computePixels')
+      sk.loadPixels();
+      let pixeldata = []
+      for (var y = 0; y < sk.height; y++){
+        for (var x = 0; x < sk.width; x++){
+          var index = (x + y * sk.width) * 4;
+          /*sk.pixels[index+0] = 0;
+          sk.pixels[index+1] = sk.random(255);
+          sk.pixels[index+2] = 0;
+          sk.pixels[index+3] = 255;*/
+          pixeldata.push(`${sk.pixels[index+0]};${sk.pixels[index+1]};${sk.pixels[index+2]}`)
+
+        }
+      }
+      //sk.updatePixels();
+      //console.log(this.sortedPixels(pixeldata));
+
+      let pixels = this.sortedPixels(pixeldata)
+      let entries = Object.entries(pixels)
+      let sorted = entries.sort((a, b) => b[1] - a[1])
+      let object = sorted.map(pixelVal => {
+        let color = pixelVal[0].split(";")
+        return {
+          color: `rgb(${color.join(",")})`,
+          val: pixelVal[1],
+          percent: (pixelVal[1] / (this.width * this.height) ) * 100
+        }
+      })
+      let shortend = object.filter(color => color.percent > 0.5)
+      this.imageData = shortend
+    },
+   sortedPixels: function(array) {
+      let result = {}
+      for (let i = 0; i < array.length; ++i) { // loop over array
+        if (!result[array[i]]){  // if no key for that number yet
+          result[array[i]] = 0;  // then make one
+        }
+        ++result[array[i]];     // increment the property for that number
+      }
+      return result
+    },
   }
 }
 </script>
+
+<style>
+  * {
+    box-sizing: border-box;
+  }
+  .home {
+    display: flex;
+  }
+  .colorBars {
+    margin: 0 2rem;
+    outline: 1px solid black;
+    background: darkgray;
+    padding: 1rem;
+    width: 20vw;
+    height: 650px;
+    overflow: scroll;
+  }
+  .colorBar {
+    text-align: left;
+    color: white;
+    padding: 0.2rem;
+  }
+  .paintControllBar {
+    min-width: 2rem;
+    margin: 0 2rem;
+    outline: 1px solid black;
+    height: 650px;
+    padding: 1rem;
+  }
+  input[type=range][orient=vertical]
+  {
+    writing-mode: bt-lr; /* IE */
+    -webkit-appearance: slider-vertical; /* WebKit */
+    width: 8px;
+    height: 175px;
+    padding: 0 5px;
+  }
+  .squareButton {
+    padding: 0.25rem;
+    margin: 0.25rem;
+    text-align: center;
+    outline: black 1px solid;
+  }
+  .squareButton:hover {
+    color: white;
+    background: darkgray;
+  }
+</style>
