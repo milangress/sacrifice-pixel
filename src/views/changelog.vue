de8bb3ed7f9c9461ca3dbaeb0a6f50653c2cf6e9
Milan G <dev@milangress.de>
2020-05-03 20:49:49 +0200

    ⚡︎ updated changelog

--- src/views/changelog.vue
+++ src/views/changelog.vue
@@ -1,12 +1,16 @@
 <template lang='pug'>
     div.changelist
         section
+            h2 fuck the versioning
+            p this is the first (working)? version. <br> the Cleanup button can now remove Pixel that are not found in the source image
+            a(href="https://acrifice-cbpvj6zz1.now.sh") sacrifice-cbpvj6zz1.now.sh
+        section
             h2 version 0.0.2
-            p updated to PUG
-            a(href="http://sacrifice-gk4bcmov6.now.sh") sacrifice-gk4bcmov6.now.sh
+            p updated to PUG and nice stuff…
+            a(href="https://sacrifice-gk4bcmov6.now.sh") sacrifice-gk4bcmov6.now.sh
         section
             h2 version 0.0.1
-            a(href="http://sacrifice-gk4bcmov6.now.sh") sacrifice-gk4bcmov6.now.sh
+            a(href="https://sacrifice-gk4bcmov6.now.sh") sacrifice-gk4bcmov6.now.sh
 </template>
 
 <script>
