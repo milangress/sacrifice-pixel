18faaf24befc0fa0aa2f89f7e4ffe98788ae4fb7
Milan G <dev@milangress.de>
2020-05-03 21:04:45 +0200

    ⚡︎ updated changelog

--- src/views/changelog.vue
+++ src/views/changelog.vue
@@ -4,9 +4,13 @@
         a(href="https://sacrifice.now.sh/") sacrifice.now.sh
         h1 Log
         section
+            h2 Tools
+            p New tools and (slightly) cleaner code 👯‍♀️
+            a(href="https://sacrifice-qnuta6hvd.now.sh") sacrifice-qnuta6hvd.now.sh
+        section
             h2 fuck the versioning?
             p this is the first (working)? version. <br> the Cleanup button can now remove Pixel that are not found in the source image
-            a(href="https://acrifice-cbpvj6zz1.now.sh") sacrifice-cbpvj6zz1.now.sh
+            a(href="https://sacrifice-cbpvj6zz1.now.sh") sacrifice-cbpvj6zz1.now.sh
         section
             h2 version 0.0.2
             p updated to PUG and nice stuff…
