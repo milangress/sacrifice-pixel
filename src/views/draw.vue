b2599aefb820e426ff7ad40e5eb2cc6c24da684e
Milan G <dev@milangress.de>
2020-05-17 16:14:38 +0200

    ⚡︎ dedicated drawing page

---
+++ src/views/draw.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+    
+</template>
+
+<script>
+    export default {
+        name: "draw",
+        computed: {
+            width () {
+                return this.$store.state.width
+            },
+            height () {
+                return this.$store.state.height
+            },
+            sacrificedPixels () {
+                return this.$store.state.pixels
+            }
+        }
+    }
+</script>
+
+<style scoped lang="stylus">
+
+</style>
