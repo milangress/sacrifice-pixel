70c5d64ebe7016a0f3dd8123fbb251801e3c3aca
Milan G <dev@milangress.de>
2020-06-10 14:46:44 +0200

    statement fix

--- src/views/Statement.vue
+++ src/views/Statement.vue
@@ -1,8 +1,8 @@
 <template lang="pug">
-    div
+    div.statement
         p In the old world, data was just a byproduct of our lives. Wherever we went we "created" data and almost none of it was ever removed. Almost every step we took was recorded and created a new bit somewhere over the rainbow in the "clouds" above us. Only to be be then soon cached (copied) by a contend delivery networks to other clouds physically near to the "user". No thought was wasted about these big data clouds and nobody expected the rain! (1)
         p But with the advent of a environmentally sustainable lifestyle some understood the value of sacrificing these bits before they could rot. (2) While normally they would be almost magically created we realized that there was a worth in reusing or composting every single pixel – to create something new from the old.
-        p This website is a a tool that allows you to sacrifice an image from your webcam, split it into its pixels and create a new image using only these pixels. By reducing the "usefulness" and the control over the output of the tool one can experience how a digital image works. Experiment what the impact of each pixel on the image is understand how every pixel is in fact sacred. So remember to not lose your face when Theseus repairs his ship. (3)
+        p This website is a a tool that allows you to sacrifice an image from your webcam, split it into its pixels and create a new image using only these pixels. By reducing the "usefulness" of the tool and the control over the output one can experience how a digital image works. Experiment what impact each pixel can have on the image und understand how every pixel is in fact sacred. So remember to not lose your face when Theseus repairs his ship. (3)
         p Let the pixel be reborn!
         hr
         p Worth a read:
@@ -18,5 +18,7 @@
 </script>
 
 <style scoped>
-
+    .statement {
+        max-width: 45ch;
+    }
 </style>
