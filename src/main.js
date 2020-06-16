6c631d6de913e7d2460d31586de68cdb0f88991d
Milan G <dev@milangress.de>
2020-06-16 16:06:36 +0200

    add google analytics

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueGtag from "vue-gtag";
+
+Vue.use(VueGtag, {
+  config: { id: "G-6Y1NN08582" }
+});
 
 Vue.config.productionTip = false
 
